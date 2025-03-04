965f15328a28786e6b66330da17fd4881a07c971
DavideCapuzz <3d.davidecapuzzo@gmail.com>
2025-03-03 21:10:22 -0600

    test post

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,10 +179,19 @@
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+      rect.bottom <= $(window).height() && /* or  */
+      rect.right <= $(window).width()  /* or */
     );
   }
+  // function isElementInViewport(el) {
+  //   var rect = el[0].getBoundingClientRect();
+  //   return (
+  //     rect.top >= 0 &&
+  //     rect.left >= 0 &&
+  //     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+  //     rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+  //   );
+  // }
 
   // Function to add an element if in mobile mode and the About section is in the viewport
   function addElementIfMobileAndInAbout() {
