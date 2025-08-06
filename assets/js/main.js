43730bdbed0607cb3c224855c1eaf7e67396753f
Davide <3d.davidecapuzzo@gmail.com>
2025-08-06 18:49:29 -0500

    style fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,21 +398,21 @@
     }
   });
 
-  var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+//   var dropdown = document.getElementsByClassName("dropdown-btn");
+// var i;
 
-for (i = 0; i < dropdown.length; i++) {
- console.log("oh")
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-} 
+// for (i = 0; i < dropdown.length; i++) {
+//  console.log("oh")
+//   dropdown[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var dropdownContent = this.nextElementSibling;
+//     if (dropdownContent.style.display === "block") {
+//       dropdownContent.style.display = "none";
+//     } else {
+//       dropdownContent.style.display = "block";
+//     }
+//   });
+// } 
 
 })(jQuery);
 
