fa3044095a44de841587dd286639dd77fea9d23f
Davide <3d.davidecapuzzo@gmail.com>
2025-08-06 18:38:23 -0500

    add dynamic dropdown menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,6 +398,22 @@
     }
   });
 
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+ console.log("oh")
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+} 
+
 })(jQuery);
 
 function createPostCard(imageUrl, Role, Company, tag, descriptionText, Data, Loc) {
